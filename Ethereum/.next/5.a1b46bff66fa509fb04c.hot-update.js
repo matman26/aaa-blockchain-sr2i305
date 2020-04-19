7cf9815329b3d03a797d877d63764f8c5d8195f2
Laura Ceron <lpceronm@unal.edu.co>
2020-04-20 01:58:29 +0200

    Initial login page

---
+++ Ethereum/.next/5.a1b46bff66fa509fb04c.hot-update.js
@@ -0,0 +1,36 @@
+webpackHotUpdate(5,{
+
+/***/ 1301:
+/***/ (function(module, exports) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: Adjacent JSX elements must be wrapped in an enclosing tag (14:6)\n\n\u001b[0m \u001b[90m 12 | \u001b[39m      \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mHead\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 13 | \u001b[39m      \n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 14 | \u001b[39m      \u001b[33m<\u001b[39m\u001b[33mHeader\u001b[39m\u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m    | \u001b[39m      \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 15 | \u001b[39m      \u001b[90m// {props.children}\u001b[39m\n \u001b[90m 16 | \u001b[39m    \u001b[90m// </Container>  \u001b[39m\n \u001b[90m 17 | \u001b[39m  )\u001b[0m\n");
+
+/***/ }),
+
+/***/ 1302:
+false,
+
+/***/ 156:
+false,
+
+/***/ 157:
+false,
+
+/***/ 158:
+false,
+
+/***/ 159:
+false,
+
+/***/ 191:
+false,
+
+/***/ 192:
+false,
+
+/***/ 81:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNS5hMWI0NmJmZjY2ZmE1MDlmYjA0Yy5ob3QtdXBkYXRlLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0EiLCJzb3VyY2VSb290IjoiIn0=
--- Ethereum/pages/login.js
+++ Ethereum/pages/login.js
@@ -37,11 +37,13 @@ class Login extends Component{
   render(){
     return(
       <Layout>
-        <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
-          <Grid.Column style={{ maxWidth: 450 }}>
+        <Grid style={{ height: '80vh' }} verticalAlign='middle' columns='equal'>
+          <Grid.Column style={{ maxWidth: 450 }} floated='left'>
+            <video width='450px' height='150px' margin='auto' src="https://d33wubrfki0l68.cloudfront.net/392e4f97600667731f325389608f9d81e23d949e/5e764/ethereum-hero-light.mp4" playsInline autoPlay loop muted>
+            </video>
             <Header as='h2' color='blue' textAlign='center'>
-              {/* <Image src='/logo.png' /> Log-in to your account */}
-              Log-in to your account
+            <Header.Content>Log-in to your account</Header.Content>
+              
             </Header>
             <Form size='large' onSubmit={this.onSubmit} error={!!this.state.errorMess}>
               <Segment stacked>
@@ -61,6 +63,34 @@ class Login extends Component{
                   compact
                 />
                 <Button color='blue' fluid size='large' type='submit' loading={this.state.loading}>
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+          <Grid.Column style={{ maxWidth: 450 } } floated='right'>
+          <Image centered size='small' src="/static/eosio.png" />
+            <Header as='h2' color='blue' textAlign='center'>
+              <Header.Content>Log-in to your account</Header.Content>
+            </Header>
+            <Form size='large'>
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon='user'
+                  iconPosition='left'
+                  placeholder='Username'
+                  onChange={event => 
+                    this.setState({username: event.target.value})
+                  }
+                />
+                {/* <Message 
+                  error 
+                  header='Something went wrong' 
+                  content={this.state.errorMess} 
+                  compact
+                /> */}
+                <Button color='blue' fluid size='large' type='submit'>
                   Login
                 </Button>
               </Segment>
