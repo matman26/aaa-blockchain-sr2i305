fb8a0cbbb262ff0f0ec01244e0f6a4b1fb87e00a
Trajanus <a.matheus26@hotmail.com>
2020-04-20 10:43:42 +0200

    Updating account name

--- docker/eos-node/bootstrap.sh
+++ docker/eos-node/bootstrap.sh
@@ -2,25 +2,26 @@
 # Start Nodeos in the background
 nohup nodeos 2>&1 1>/root/nodeos.log &
 
-sleep 5
+sleep 2
 
+PARAMS='-u http://172.18.0.2:8888 '
 # This creates an empty wallet; by default wallets are unlocked
-cleos wallet create -n user --to-console
+cleos ${PARAMS} wallet create -n user --to-console
 
 # This imports a given private key to the wallet, thus linking it to a key pair
-cleos wallet import -n user \
+cleos ${PARAMS} wallet import -n user \
 	--private-key "${PRIVATE_KEY}"
 
 # Create two blockchain accounts
-cleos create account eosio provider "${PUBLIC_KEY}"
-cleos create account eosio matheus "${PUBLIC_KEY}"
+cleos ${PARAMS} create account eosio aaa "${PUBLIC_KEY}"
+cleos ${PARAMS} create account eosio matheus "${PUBLIC_KEY}"
 
-# Deploy the AAA contract to the provider account
-cleos set contract provider /root/aaa -p provider@active
+# Deploy the AAA contract to the aaa account
+cleos ${PARAMS} set contract aaa /root/aaa -p aaa@active
 
 # Push data to contract, change user's role
-cleos push action provider adduser '["matheus"]' -p matheus@active
-cleos push action provider setrole '["matheus",1]' -p provider@active
+cleos ${PARAMS} push action aaa adduser '["matheus"]' -p matheus@active
+cleos ${PARAMS} push action aaa setrole '["matheus",1]' -p aaa@active
 
 /bin/sh
 
